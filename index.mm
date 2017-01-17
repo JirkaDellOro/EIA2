e2376e3e92de24cbe77d400a43a11b349810d1f7
Jirka <del@hs-furtwangen.de>
2017-01-17 12:43:09 +0100

    added index.mm

---
+++ index.mm
@@ -0,0 +1,6 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1484653276160" ID="ID_1326062048" MODIFIED="1484653284480" TEXT="index">
+<node CREATED="1484653286890" ID="ID_1533935581" LINK="DO-Hierarchy.pdf" MODIFIED="1484653330770" POSITION="right" TEXT="Document Object Hierarchy"/>
+</node>
+</map>
